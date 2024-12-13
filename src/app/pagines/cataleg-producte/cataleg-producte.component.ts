e2f69f4560d5899e1524fdffb8fd51b9525e6bfc
curdu <curdu01@gmail.com>
2024-12-13 17:14:15 +0100

    Modificat el component CatalegProducteComponent per mostrar el producte segons un id especificat a l'URL

--- src/app/pagines/cataleg-producte/cataleg-producte.component.ts
+++ src/app/pagines/cataleg-producte/cataleg-producte.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductesService} from '../../serveis/productes.service';
+import {ActivatedRoute} from '@angular/router';
+import {Producte} from '../../model/Producte';
 
 @Component({
   selector: 'app-cataleg-producte',
@@ -6,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './cataleg-producte.component.html',
   styleUrl: './cataleg-producte.component.css'
 })
-export class CatalegProducteComponent {
+export class CatalegProducteComponent implements OnInit {
+
+  textHTML!: HTMLElement;
+  productesServeis: ProductesService;
+  route: ActivatedRoute;
+  producte!: Producte;
+
+  constructor(productesServei: ProductesService, route: ActivatedRoute) {
+    this.productesServeis = productesServei;
+    this.route = route;
+
+  }
+
+  ngOnInit():void {
+    this.textHTML = document.getElementById('text')!;
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.producte = this.productesServeis.getProducteById(id)!;
+    this.textHTML.innerText = this.producte.toString();
+
+  }
+
 
 }
