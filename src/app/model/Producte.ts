1032c4375652b58abf1fe6741ea0c41e6094437e
curdu <curdu01@gmail.com>
2024-12-13 17:08:08 +0100

    Creada la classe de Producte a src/model/Producte.ts amb atributs i constructor

---
+++ src/app/model/Producte.ts
@@ -0,0 +1,24 @@
+export class Producte {
+  id: number;
+  nom: string;
+  imageUrl: string;
+  preu: number;
+  description: string;
+  categoria: string
+  constructor(id: number, nom: string, imageUrl: string, preu: number, description: string, categoria: string) {
+    this.id = id;
+    this.nom = nom;
+    this.imageUrl = imageUrl;
+    this.preu = preu;
+    this.description = description;
+    this.categoria = categoria;
+  }
+
+  toString(): string {
+    return `Producte{id: ${this.id}, nom: ${this.nom}, imageUrl: ${this.imageUrl}, preu: ${this.preu}, description: ${this.description}}`
+  }
+
+
+
+
+}
