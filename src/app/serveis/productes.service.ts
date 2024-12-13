25542243c8a0b7bfd5ad9ce3614eea03d5a0c5e5
curdu <curdu01@gmail.com>
2024-12-13 17:12:56 +0100

    Creat el servei ProductesService que carrega varis productes i afegit 2 funcions, una per obtenir les categories i una altra per obtenir un producte per ID

---
+++ src/app/serveis/productes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {Producte} from '../model/Producte';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductesService {
+
+  productes: Producte[];
+
+  constructor() {
+    this.productes = [];
+    this.generateProductes();
+
+
+
+
+  }
+
+  private generateProductes(): void {
+    this.productes.push(new Producte(0,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
+    this.productes.push(new Producte(1,"Dessuadora Bulbasaur","assets/dessuadorabulbasaur.png",19.2,"BOmboclat","Dessuadora"))
+    this.productes.push(new Producte(2,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
+    this.productes.push(new Producte(3,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
+    this.productes.push(new Producte(4,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
+  }
+  getAllCategories(): String[] {
+    let categories: String[] = [];
+    for (const producte of this.productes) {
+      if(!categories.includes(producte.categoria)){
+        categories.push(producte.categoria);
+      }
+    }
+    return categories;
+  }
+  getProducteById(id: number): Producte | undefined {
+    return this.productes.find((producte) => producte.id === id);
+  }
+}
