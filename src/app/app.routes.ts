8e12178dd9a7fab94904d06661627b12c73c800a
curdu <curdu01@gmail.com>
2024-12-13 13:01:57 +0100

    Afegits els components usats per mostrar les pagines i associats amb el routing a app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {CatalegComponent} from './pagines/cataleg/cataleg.component';
+import {CatalegProducteComponent} from './pagines/cataleg-producte/cataleg-producte.component';
+import {CatelegCategoriaComponent} from './pagines/cateleg-categoria/cateleg-categoria.component';
+import {CondidionsComponent} from './pagines/condidions/condidions.component';
+import {ContacteComponent} from './pagines/contacte/contacte.component';
+import {IniciSessioComponent} from './pagines/inici-sessio/inici-sessio.component';
+import {RegistreComponent} from './pagines/registre/registre.component';
+import {IniciComponent} from './pagines/inici/inici.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: IniciComponent },
+  {path: 'inici', component: IniciComponent},
+  {path: 'cataleg', component: CatalegComponent},
+  {path: 'cataleg/producte/:id', component: CatalegProducteComponent},
+  {path: 'cataleg/categoria/:cat', component: CatelegCategoriaComponent},
+  {path: 'condicions', component: CondidionsComponent},
+  {path: 'contacte', component: ContacteComponent},
+  {path: 'inicisessio', component: IniciSessioComponent},
+  {path: 'registre', component: RegistreComponent},
+];
