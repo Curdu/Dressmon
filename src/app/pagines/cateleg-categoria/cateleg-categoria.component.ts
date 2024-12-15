4240fa3ca1dca0b62eeb59ba5093c37476534bf8
curdu <curdu01@gmail.com>
2024-12-15 12:50:52 +0100

    Modificat CatalegCategoriaComponent per mostrar els productes d'una categoria

--- src/app/pagines/cateleg-categoria/cateleg-categoria.component.ts
+++ src/app/pagines/cateleg-categoria/cateleg-categoria.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductesService} from '../../serveis/productes.service';
+import {ActivatedRoute} from '@angular/router';
+import {Producte} from '../../model/Producte';
 
 @Component({
   selector: 'app-cateleg-categoria',
@@ -6,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './cateleg-categoria.component.html',
   styleUrl: './cateleg-categoria.component.css'
 })
-export class CatelegCategoriaComponent {
+export class CatelegCategoriaComponent implements OnInit {
+
+  productes: Producte[];
+  categoria: string | null;
+
+  constructor(private producteService: ProductesService, private route: ActivatedRoute) {
+    this.categoria = "";
+    this.productes = [];
+  }
+
+
+
+  ngOnInit():void {
+    this.categoria = this.route.snapshot.paramMap.get('cat')!;
+    this.productes = this.producteService.getProductesByCategoria(this.categoria)
+
+
+  }
 
 }
