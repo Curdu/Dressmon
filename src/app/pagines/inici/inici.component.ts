25d55582ee06e16522a0d59bd2276277a646bd8e
curdu <curdu01@gmail.com>
2024-12-20 16:00:26 +0100

    Retocs de l'estètica del footer

--- src/app/pagines/inici/inici.component.ts
+++ src/app/pagines/inici/inici.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {NavigationComponent} from '../../components/navigation/navigation.component';
+
 
 @Component({
   selector: 'app-inici',
   imports: [
-    NavigationComponent
+
   ],
   templateUrl: './inici.component.html',
   styleUrl: './inici.component.css'
