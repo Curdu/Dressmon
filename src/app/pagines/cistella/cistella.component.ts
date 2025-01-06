0e246f0d50b8923293ba126f0f5fdc8174da3507
curdu <curdu01@gmail.com>
2025-01-06 19:12:27 +0100

    Creada la pàgina de cistella (ens n'havíem oblidat (; )

---
+++ src/app/pagines/cistella/cistella.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cistella',
+  imports: [],
+  templateUrl: './cistella.component.html',
+  styleUrl: './cistella.component.css'
+})
+export class CistellaComponent {
+
+}
