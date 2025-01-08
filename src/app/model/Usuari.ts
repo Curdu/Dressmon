06f0d1c8bf988b4396837c48ee6546fe62a73af4
curdu <curdu01@gmail.com>
2025-01-08 16:14:54 +0100

    Creada la classe usuari a Usuari.ts

---
+++ src/app/model/Usuari.ts
@@ -0,0 +1,17 @@
+import {Producte} from './Producte';
+
+export class Usuari{
+  nom: string;
+  correu: string;
+  passwd: string;
+  cistella: Map<Producte, number>;
+
+  constructor(nom: string, correu: string, passwd: string) {
+    this.nom = nom;
+    this.correu = correu;
+    this.passwd = passwd;
+    this.cistella = new Map<Producte, number>();
+  }
+
+
+}
