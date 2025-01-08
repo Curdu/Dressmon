e83fd6dff15bb565c0d5577903069c51cfb915bc
curdu <curdu01@gmail.com>
2025-01-08 18:33:48 +0100

    Funcionalitat de registre d'usuari feta //TODO fer-ho maco

--- src/app/pagines/registre/registre.component.ts
+++ src/app/pagines/registre/registre.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {UsuarisService} from '../../serveis/usuaris.service';
+import {Usuari} from '../../model/Usuari';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-registre',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './registre.component.html',
   styleUrl: './registre.component.css'
 })
 export class RegistreComponent {
 
+  nom : string;
+  correu: string;
+  passwd: string;
+  passwdConfirm : string;
+
+  constructor(private router: Router, private s: UsuarisService) {
+
+    this.nom = "";
+    this.correu = "";
+    this.passwd = "";
+    this.passwdConfirm  = "";
+
+  }
+
+  registrarUsuari(){
+
+    if(this.esContrasenyaConfirmada()){
+      if(this.s.registrarUsuari(new Usuari(this.nom, this.correu, this.passwd))){
+        this.router.navigate(['inicisessio']);
+      }else {
+        console.log("Aquest usuari ja existeix")
+      }
+    }else{
+      console.log("Les contrasenyes no coincideixen")
+    }
+  }
+
+  private esContrasenyaConfirmada(): boolean{
+    return this.passwd === this.passwdConfirm;
+  }
 }
