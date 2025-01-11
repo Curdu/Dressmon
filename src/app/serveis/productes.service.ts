6c85caa8d88ef9dff64d828ba8d230ebe65d468d
curdu <curdu01@gmail.com>
2025-01-11 14:47:13 +0100

    Eliminat producte amb id 0 i ajustat el tamany dels productes

--- src/app/serveis/productes.service.ts
+++ src/app/serveis/productes.service.ts
@@ -15,9 +15,9 @@ export class ProductesService {
   }
 
   private generateProductes(): void {
-    this.productes.push(new Producte(0,"Mitjons Bulbasaur","/assets/productes/sudadera-bulbasaur-600x600.jpg",19.2,"BOmboclat","Mitjons"))
+
     this.productes.push(new Producte(1,"Dessuadora Bulbasaur","/assets/productes/sudadera-bulbasaur-600x600.jpg",19.2,"BOmboclat","Dessuadora"))
-    this.productes.push(new Producte(2,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
+    this.productes.push(new Producte(2,"Mitjons Bulbasaur","/assets/productes/mitjonsBulbasaur.jpg",19.2,"BOmboclat","Mitjons"))
     this.productes.push(new Producte(3,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
     this.productes.push(new Producte(4,"Mitjons Bulbasaur","assets/mitjonsbulbasaur.png",19.2,"BOmboclat","Mitjons"))
   }
