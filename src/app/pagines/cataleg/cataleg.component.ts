f4a3d7c50dd958b057ca2a298c8a424b77291c21
MarcBota <marc.bota@institutvidreres.cat>
2025-01-12 20:39:48 +0100

    la part del catalag amb les imatges necessaries

--- src/app/pagines/cataleg/cataleg.component.ts
+++ src/app/pagines/cataleg/cataleg.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {ProductesService} from '../../serveis/productes.service';
 
 @Component({
   selector: 'app-cataleg',
   imports: [],
   templateUrl: './cataleg.component.html',
+  standalone: true,
   styleUrl: './cataleg.component.css'
 })
 export class CatalegComponent {
+  llistaCategories: string[]
+  constructor(private productesServei: ProductesService) {
+    this.llistaCategories = productesServei.getAllCategories()
+  }
 
 }
