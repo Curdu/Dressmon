b59f3904ccff11e6b15b096b024813c2465dca69
curdu <curdu01@gmail.com>
2025-02-14 23:11:51 +0100

    Afegida la funció de mostrar un pokemon aleatori a la pàgina de inici

--- src/app/pagines/inici/inici.component.ts
+++ src/app/pagines/inici/inici.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {JSONPath} from 'jsonpath-plus';
 
 
 @Component({
@@ -9,6 +11,35 @@ import { Component } from '@angular/core';
   templateUrl: './inici.component.html',
   styleUrl: './inici.component.css'
 })
-export class IniciComponent {
+export class IniciComponent implements OnInit {
+    imatgePokemon: string;
+    nomPokemon: string;
+
+    constructor(private http: HttpClient) {
+      this.imatgePokemon = '';
+      this.nomPokemon = '';
+    }
+    async ngOnInit() {
+      await this.carregarPokemon().then((response)=>{
+        this.imatgePokemon = JSONPath({path: "$.sprites.other.official-artwork.front_default", json: response})[0];
+        this.nomPokemon = JSONPath({path: "$.forms.[0].name", json: response})[0].toUpperCase();
+
+      })
+
+    }
+
+    carregarPokemon() {
+      return new Promise<any>(resolve => {
+        this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${this.numeroRandom()}`).subscribe(
+          (res) =>{
+            resolve(res);
+          }
+        )
+      })
+    }
+
+    numeroRandom(): number {
+      return Math.floor(Math.random() * 1024);
+    }
 
 }
