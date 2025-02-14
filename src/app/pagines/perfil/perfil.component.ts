dcf0630d110d323f6120580348d2815096eb846f
curdu <curdu01@gmail.com>
2025-02-14 22:43:52 +0100

    Afegit un log out a la pàgina perfil

--- src/app/pagines/perfil/perfil.component.ts
+++ src/app/pagines/perfil/perfil.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {UsuarisService} from '../../serveis/usuaris.service';
 import {FormsModule} from '@angular/forms';
 import {Usuari} from '../../model/Usuari';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -15,7 +16,7 @@ export class PerfilComponent implements OnInit{
 
   usuari: Usuari;
 
-  constructor(private s : UsuarisService) {
+  constructor(private s : UsuarisService, private router: Router) {
     this.usuari = this.s.getUsuariActiu();
     console.log(this.usuari);
   }
@@ -24,6 +25,12 @@ export class PerfilComponent implements OnInit{
   }
   async modificarUsuari(){
     await this.s.modificarUsuari(this.usuari).then()
+  }
+  async tancarSessio(){
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('usuariActiu');
+    await this.router.navigate(['/inici'])
+
   }
 
 }
